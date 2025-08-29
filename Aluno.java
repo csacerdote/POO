367df8f731532cf304f43e3981425fe07f181b9d
Claudio <25777574+csacerdote@users.noreply.github.com>
2025-08-29 20:48:59 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estudos;
+
+/**
+ *
+ * @author claudio
+ */
+public class Aluno extends Pessoa {
+    private String matricula;
+
+    public String matricula() {
+        return matricula;
+    }
+
+    public void matricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
+    
+}
