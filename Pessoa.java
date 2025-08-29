41706690854ab624ca8db0c976fad4d3600ec070
Claudio <25777574+csacerdote@users.noreply.github.com>
2025-08-29 20:48:38 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estudos;
+/**
+ *
+ * @author claudio
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+}
